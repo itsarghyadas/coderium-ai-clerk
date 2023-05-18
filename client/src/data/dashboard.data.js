6d339096b44fcbcbf9fe6bfa9f20efa7c9f376a5
Arghya Das <arghyadasproject@gmail.com>
2023-05-18 09:12:54 +0530

    image path updated

--- client/src/data/dashboard.data.js
+++ client/src/data/dashboard.data.js
@@ -3,7 +3,8 @@ export default [
     id: "CODERIUM - Just like ChatGPT",
     version: "v1.0.1",
     title: "CODERIUM",
-    image: "../../src/assets/coderium-ai-chat.png",
+    image:
+      "https://res.cloudinary.com/dzzhtacky/image/upload/v1684381191/coderium-ai-chat_ll5w8s.png",
     details:
       "We have used OpenAI API to make a chatbot like ChatGPT which can answer followup questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests.",
     linkName: "Try Coderium",
@@ -23,7 +24,8 @@ export default [
     id: "IMAGIPIX - Just like DALL-E",
     version: "v1.0",
     title: "IMAGIPIX",
-    image: "../../src/assets/dall-e-ui.png",
+    image:
+      "https://res.cloudinary.com/dzzhtacky/image/upload/v1684381192/dall-e-ui_brkowg.png",
     details:
       "We have used OpenAI API DALL-E model to make an AI Art Generator with prompt like DALL-E.2 which can generate AI Art based on the prompt. And also it can edit your existing images which is a great feature but not available for now in this version.",
     linkName: "Try Imagipix",
